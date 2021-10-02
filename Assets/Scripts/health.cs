9eeeaf4c555dee9f96e2da0499f667e8483f6cd1
Андрей Выдря <65971311+Ankrowi@users.noreply.github.com>
2021-10-02 14:11:22 +0300

    health

---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+
+    public int max_health;
+    private int _current_health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _current_health = max_health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // метод возвращает, жив ли объект после получения урона
+    public bool get_damage(int damage_rate)
+    {
+        _current_health -= damage_rate;
+        if (_current_health > 0) return true;
+        else return false;
+    }
+
+    public void get_heal(int heal_rate)
+    {
+        _current_health += heal_rate;
+        if (_current_health > max_health) _current_health = max_health;
+    }
+}
