7e136dc3ad6ba9cf178e22c669f2704475f6e2b8
Schikasi <35520439+Schikasi@users.noreply.github.com>
2021-10-02 21:00:01 +0300

    Камера

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = Player.transform.position;
+        pos.z = -10;
+        transform.position = pos; 
+
+
+    }
+}
