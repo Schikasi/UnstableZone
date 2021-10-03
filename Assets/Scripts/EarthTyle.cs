dd66f14a7c31d461a583db737db9898c13ca3c66
Андрей Выдря <65971311+Ankrowi@users.noreply.github.com>
2021-10-03 21:34:47 +0300

    TyleTypes

---
+++ Assets/Scripts/EarthTyle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthTyle : MonoBehaviour, ITyleType
+{
+    // Start is called before the first frame update
+    public List<GameObject> _sprites;
+
+    public Sprite get_random()
+    {
+        Sprite _return = null;
+        System.Random rand = new System.Random();
+        _return = _sprites[rand.Next(0, _sprites.Count)].GetComponent<SpriteRenderer>().sprite;
+        return _return;
+    }
+}
---
+++ Assets/Scripts/ITyleType.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ITyleType
+{
+    // returns a random sprite in list
+    Sprite get_random();
+
+
+}
