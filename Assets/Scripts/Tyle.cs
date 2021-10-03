9ad00b2ce1a516fb97013a2e986b202c6da584da
Андрей Выдря <65971311+Ankrowi@users.noreply.github.com>
2021-10-03 23:55:10 +0300

    TyleScript

---
+++ Assets/Scripts/Tyle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tyle : MonoBehaviour
+{
+    public string tyle_type = "default";
+    private BoxCollider2D _bc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform formation = gameObject.GetComponent<Transform>();
+        if ( formation != null )
+        {
+            _bc = gameObject.AddComponent<BoxCollider2D>();
+            _bc.size = gameObject.GetComponent<SpriteRenderer>().size / 2;
+            _bc.isTrigger = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == this.name) return;
+        Debug.Log(other.gameObject.name + "ohuel po mne hodit'!");
+    }
+}
